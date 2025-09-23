989929ee2c45e9a77fd11091cc6ed2ceff256b87
Sahil Nair <sahiln@mail.uni-paderborn.de>
2025-09-23 16:08:03 +0200

    fixed the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ThemeStore } from '../store/themestore';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-navbar',
+  imports: [ MatIconModule, MatButtonModule],
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    isMobileMenuOpen = signal(false);
+
     public readonly themeStore = inject(ThemeStore);
+
+    toggleMobileMenu() {
+        this.isMobileMenuOpen.set(!this.isMobileMenuOpen());
+    }
 }
 
 
