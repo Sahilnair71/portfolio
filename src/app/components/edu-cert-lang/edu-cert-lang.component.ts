83a7537721ce5acc05e60ae434e0bc1a05f04dfd
Sahil Nair <sahiln@mail.uni-paderborn.de>
2025-10-01 02:33:05 +0200

    added service

--- src/app/components/edu-cert-lang/edu-cert-lang.component.ts
+++ src/app/components/edu-cert-lang/edu-cert-lang.component.ts
@@ -1,31 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { EduCertLangService } from '../../services/edu-cert-lang.service';
 
-type Education = { degree: string; school: string; year: string; location: string };
-type Certificate = { name: string; issuer: string; year: string };
-type Language = { name: string; level: string };
 
-type Payload = {
-  education: Education[];
-  certificates: Certificate[];
-  languages: Language[];
-};
 
 @Component({
   selector: 'app-edu-cert-lang',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [],
   templateUrl: './edu-cert-lang.component.html',
   styleUrl: './edu-cert-lang.component.scss'
 })
 export class EduCertLangComponent {
-  private http = inject(HttpClient);
+  private eduCertLangService = inject(EduCertLangService);
 
-  private data$ = this.http.get<Payload>('assets/backend_data/edu-cert-lang.json');
-
-  education = toSignal(this.data$.pipe(map(d => d.education)), { initialValue: [] as Education[] });
-  certificates = toSignal(this.data$.pipe(map(d => d.certificates)), { initialValue: [] as Certificate[] });
-  languages = toSignal(this.data$.pipe(map(d => d.languages)), { initialValue: [] as Language[] });
+  // 2. Expose signals directly from the service (read-only access)
+  education = this.eduCertLangService.education;
+  certificates = this.eduCertLangService.certificates;
+  languages = this.eduCertLangService.languages;
 }
